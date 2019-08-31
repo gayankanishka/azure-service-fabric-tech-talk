c546358ca2fc486149db33f61a5fb44afe73d215
Gayan Kanishka Wijetunga <32380979+gayankanishka@users.noreply.github.com>
2019-08-31 20:01:56 +0530

    renamed the solution (#3)

---
+++ examples/AzureServiceFabric.TechTalk.Ingest/AzureServiceFabric.TechTalk.Ingest/API/ApiServiceAssembly.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AzureServiceFabric.TechTalk.Ingest.API
+{
+    // Need this stub assembly 
+    public class ApiServiceAssembly
+    {
+        
+    }
+}
