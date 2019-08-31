d5f4d256a809dee50ed7cd66e4d0b3022a3db9eb
Gayan K <gkanishka6@gmail.com>
2019-08-31 16:51:02 +0530

    adding dummy comments

--- examples/AzureServiceFabric.TechTalk.Main/AzureServiceFabric.TechTalk.Main/API/Business/IIngestBusiness.cs
+++ examples/AzureServiceFabric.TechTalk.Main/AzureServiceFabric.TechTalk.Main/API/Business/IIngestBusiness.cs
@@ -3,9 +3,16 @@ using System.Threading.Tasks;
 
 namespace AzureServiceFabric.TechTalk.Main.API.Business
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public interface IIngestBusiness
     {
-        //TODO: Comments
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
         Task IngestIntoStorageAsync(Message message);
     }
 }
